06610fb3349a1f6ba32d73082a5822e1a8203add
Sajjad-Shahali <sajjadshahali97@gmail.com>
2026-03-08 05:05:33 +0100

    expired

--- movewise-react/src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card({ title, subtitle, className = "", children, onClick }) {
-  return (
-    <section className={`card ${className}`} onClick={onClick} role={onClick ? "button" : undefined} tabIndex={onClick ? 0 : undefined}>
-      {title && <h3 className="card-title">{title}</h3>}
-      {subtitle && <p className="card-subtitle">{subtitle}</p>}
-      {children}
-    </section>
-  );
-}
